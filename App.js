471e697d1495419ff106d0ce170e506ea6bbc2e4
Tugra <tugraturkseven@hotmail.com>
2023-05-17 14:01:50 +0300

    progress bar changes.

--- App.js
+++ App.js
@@ -1,29 +1,16 @@
 import { View } from 'react-native'
 import { Text, useTheme, Divider, ProgressBar, MD3Colors } from 'react-native-paper';
-import { AnimatedCircularProgress } from 'react-native-circular-progress';
+import User from './components/User';
 
 export default function App() {
   const theme = useTheme();
   return (
-    <View style={{ backgroundColor: theme.colors.onSecondaryContainer, display: 'flex', height: "100%" }}>
-      <Text variant="headlineMedium" style={{ color: theme.colors.onPrimary, display: 'flex', textAlign: 'center', marginTop: 50, }}>🔥 Fire Detector 🔥</Text>
-      <Divider style={{ marginTop: 10 }} />
-      <Text variant="titleMedium" style={{ color: theme.colors.onPrimary, display: 'flex', textAlign: 'center', marginTop: 50, }}></Text>
-      <AnimatedCircularProgress
-        size={150}
-        width={10}
-        fill={80}
-        tintColor="#D3010D"
-        padding={10}
-        backgroundColor="#3d587550" style={{ display: 'flex', justifyContent: 'center', alignContent: 'center', alignItems: 'center', }}>
-        {
-          (fill) => (
-            <Text style={{ color: theme.colors.onPrimary }} >
-              {"Smoke & Gas"}
-            </Text>
-          )
-        }
-      </AnimatedCircularProgress>
+    <View style={{ backgroundColor: theme.colors.secondaryContainer, display: 'flex', height: "100%" }}>
+      <User />
+      <Text variant="titleMedium" style={{ color: MD3Colors.primary10, display: 'flex', textAlign: 'center', marginTop: 50, }}>Smoke & Gas</Text>
+      <ProgressBar animatedValue={0.35} style={{ margin: 30, }} color={MD3Colors.error50} />
+      <Text variant="titleMedium" style={{ color: MD3Colors.primary10, display: 'flex', textAlign: 'center', marginTop: 50, }}>Is fire detected?</Text>
+      <ProgressBar animatedValue={0.35} style={{ margin: 30, }} color={'green'} />
     </View>
   );
 }
