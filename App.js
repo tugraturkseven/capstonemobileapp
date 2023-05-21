794262b124b13be4478ae8602f78b6ac851d53ef
Tugra <tugraturkseven@hotmail.com>
2023-05-21 21:14:53 +0300

    font family update, login screen ui, stack navigation.

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
-import { View, Image } from 'react-native'
-import { Text, useTheme, Avatar } from 'react-native-paper';
-import User from './components/User';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={{ backgroundColor: 'black', display: 'flex', height: "100%" }}>
-      <User ></User>
-      <Text variant="displayMedium" style={{ color: 'white', display: 'flex', textAlign: 'center', marginTop: 10, fontWeight: '300' }}>Status</Text>
-      <Avatar.Image size={72} style={{ backgroundColor: 'black', marginLeft: 'auto', marginRight: 'auto', marginTop: 30 }} source={require('../capstonemobileapp/assets/co2.png')} />
-      <Text variant='titleLarge' style={{ color: 'white', fontWeight: 100, textAlign: 'center' }}>100</Text>
-      <Avatar.Image size={72} style={{ backgroundColor: 'black', marginLeft: 'auto', marginRight: 'auto', marginTop: 125 }} source={require('../capstonemobileapp/assets/fire.png')} />
-      <Text variant='titleLarge' style={{ color: 'white', fontWeight: 100, textAlign: 'center', marginTop: 30 }}>No fire detected.</Text>
-      <Image source={require('../capstonemobileapp/assets/smoke-background.jpg')} style={{ marginTop: -40, zIndex: -1 }} />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Main"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/LoginScreen.js
@@ -0,0 +1,55 @@
+import { View, Image, SafeAreaView, StyleSheet } from 'react-native'
+import { Text, TextInput, Avatar, Button, MD3Colors } from 'react-native-paper';
+import React, { useState } from 'react';
+
+const styles = StyleSheet.create({
+    fire: {
+        zIndex: -1,
+        transform: 'rotate(180deg)',
+    },
+    roboto: {
+        fontFamily: 'Roboto',
+        fontWeight: 300,
+        color: 'white',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+    },
+    smoke: {
+        zIndex: -1,
+        height: '30%'
+    }
+});
+
+export default function LoginScreen({ navigation }) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [secureText, setSecureText] = useState(true);
+    return (
+        <SafeAreaView style={{ backgroundColor: 'black', height: '100%' }} >
+            <Image source={require('../assets/fire-background.jpg')} style={styles.fire} />
+            <View >
+                <Avatar.Icon size={136} icon="account-circle" style={{ backgroundColor: 'black', marginLeft: 'auto', marginRight: 'auto', marginTop: -200, }} />
+                <Text style={styles.roboto} variant='titleMedium'>Kullanıcı Adı</Text>
+                <TextInput value={email} onChangeText={x => setEmail(x)} outlineColor='white' mode='outlined' style={{ width: 300, height: 40, marginLeft: 'auto', marginRight: 'auto', borderColor: 'white', marginBottom: 10 }} />
+                <Text style={styles.roboto} variant='titleMedium'>Şifre</Text>
+                <TextInput
+                    secureTextEntry={secureText}
+                    value={password} onChangeText={x => setPassword(x)} outlineColor='white' mode='outlined' style={{ width: 300, height: 40, marginLeft: 'auto', marginRight: 'auto', borderColor: 'white', marginBottom: 10 }}
+                    right={<TextInput.Icon icon="eye" style={{ marginTop: 'auto', marginBottom: 'auto', marginLeft: 'auto', marginRight: 'auto' }} size={18} onPress={() => setSecureText(!secureText)} />}
+                />
+                <Button icon="login" mode="contained" style={{ width: 150, marginLeft: 'auto', marginRight: 'auto', backgroundColor: MD3Colors.neutralVariant20 }} rippleColor={MD3Colors.neutralVariant40} onPress={() => console.log('basıldı.')} >
+                    Giriş Yap
+                </Button>
+                <Button mode="text" onPress={() => console.log('text')} style={{ width: 150, marginLeft: 'auto', marginRight: 'auto', marginTop: 10 }} textColor={MD3Colors.neutral80}>
+                    Şifreni mi unuttun?
+                </Button>
+                <Button mode="text" onPress={() => console.log('text')} style={{ width: 100, marginLeft: 'auto', marginRight: 'auto', marginTop: 10 }} textColor={MD3Colors.neutral80}>
+                    Kayıt ol.
+                </Button>
+
+            </View>
+            <Image source={require('../assets/smoke-background.jpg')} style={styles.smoke} />
+        </SafeAreaView >
+    )
+}
+
